99dd03a336f022d96acb7809749f020604a8ee98
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-09-02 01:57:47 +0200

    created main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Mina</h1>
+      <Header />
+      <Nav />
+      <Body />
+      <Footer />
     </div>
   );
 }
