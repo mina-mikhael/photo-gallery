31502a8ebfc4599eed4323ac451ab7b28d7a87a8
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-09-02 02:38:30 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Body />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <Link to={"/"}> Home</Link>
+      <Link to={"/gallery"}> Gallery</Link>
+    </div>
+  );
 }
 
 export default Nav;
